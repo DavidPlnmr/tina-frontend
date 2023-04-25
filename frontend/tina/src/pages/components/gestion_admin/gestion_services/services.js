96cf3caf69af17c945f5dd48181de42b6245cfcb
Steph <stephane.bngrd@gmail.com>
2023-04-26 01:55:07 +0200

    Changement service.js
    cards + mode modification

--- frontend/tina/src/pages/components/gestion_admin/gestion_services/services.js
+++ frontend/tina/src/pages/components/gestion_admin/gestion_services/services.js
@@ -1,13 +1,20 @@
 import { useEffect, useState } from 'react';
-import Header from '../../header';
 import Link from 'next/link';
 import { parseCookies } from 'nookies';
 import axios from 'axios';
+import { Card, Button } from 'react-bootstrap';
 
 
 export default function Services() {
   const [lstServices, setLstServices] = useState([]);
   const [lstTypesOfService, setLstTypesOfService] = useState([]);
+  const [buttonModify, setButtonModify] = useState(
+    "btn btn-outline-dark"
+  );
+  const [modificationMode, setModificationMode] = useState(
+    ""
+  );
+  const [mode, setMode] = useState(true);
 
   const fetchTypeOfService = () => {
         const cookies = parseCookies();
@@ -39,94 +46,143 @@ export default function Services() {
               console.log(error);
           });
   };
-  const loadComponents = () => {
-    let lstComponents = [];
-    lstServices.map((s) => {
-      lstComponents.push(      
-        <div class="row">
-          <p>{s.name}</p>
-          <p>{s.description}</p>
-          <p>{s.price}</p>
-          <p>{s.duration}</p>
-          <p>{s.type_of_service}</p>
-        </div>
-      );
-    });
-    return lstComponents;
+
+  const handleClickModify = (evt) => {
+    console.log("Mode Modification");
+    setMode(mode =>!mode);
   };
+
+
+  //Partie du code de milaz
+
+  // Function to format the duration
+  const formatDuration = (duration) => {
+    const hours = duration.split(":")[0] * 60;
+    const minutes = duration.split(":")[1];
+    return parseInt(hours) + parseInt(minutes);
+};
+  // Function to get price without cents
+  const priceWithoutCent = (price) => {
+    return price.split(".")[0];
+}
+  // Setting number of columns for services
+  const numCols = lstTypesOfService ? Math.floor(12 / lstTypesOfService.length) : 4;
+  // Function to get the minimum price for a type of service
+  const minPriceForATypeOfService = (lstTypeOfService) => {
+    return priceWithoutCent
+        (lstServices
+        .filter((service) => service.type_of_service === lstTypeOfService.id)
+        .sort((a, b) => a.price - b.price)[0].price);
+}
 
   useEffect(() => {
     fetchServices();
     fetchTypeOfService();
+    if (mode) {
+      //changer le bouton
+      setButtonModify(buttonModify=>"btn btn-outline-dark");
+      //changer la navbar
+      setModificationMode(modificationMode => "");
+    } else {
+      //changer le bouton
+      setButtonModify(buttonModify => "btn btn-dark");
+      //changer la navbar
+      setModificationMode(modificationMode=>"MODE MODIFICATION");
+    }
 
-  }, []);
+  }, [mode]);
 
   return (
     <>
+      {/* Nav Bar pour les services */}
       <div className="d-flex flex-column justify-content-start align-items-center" style={{ height: "100vh", backgroundColor: "#b8aaa0" }}>
         <ul></ul>
         <nav class="navbar navbar-expand-lg bg-body-tertiary" style={{backgroundColor:"#b8aaa0"}}>
-          <div class="container-fluid" style={{ height: "8vh", width:"100vh", backgroundColor: "#FFFFFF" }}>
-              <a class="navbar-brand" href="#">Gestion des services</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              </ul>
-              <button type="button" class="btn btn-outline-dark">Options</button>
-              <ul class="navbar-nav me-2 mb-2 mb-lg-0">
-              </ul>
-              <button type="button" class="btn btn-outline-dark">Modifier</button>
-              <ul class="navbar-nav me-2 mb-2 mb-lg-0">
-              </ul>
-              <button type="button" class="btn btn-primary">
-                <Link href="/components/gestion_admin/gestion_services/form_typesofservice" class="nav-link">
-                  Ajouter
-                </Link>
-              </button>
-                
+          <div class="container-fluid text-center rounded" style={{ height: "8vh", width:"100vh", backgroundColor: "#FFFFFF" }}>
+            <div class="collapse navbar-collapse" id="text">
+              <a class="navbar-brand">Gestion des services</a>
+              <ul class="navbar-nav ms-auto mb-5 ms-lg-3"></ul>
+              <i class="navbar-text">
+                {modificationMode}
+              </i>
+            </div>
+            <div class="collapse navbar-collapse" id="buttons">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0"></ul>
+                <button type="button" class={buttonModify} onClick={handleClickModify}>Modifier</button>
+                <button type="button" class="btn btn-primary ms-lg-2">
+                  <Link href="/components/gestion_admin/gestion_services/form_typesofservice" class="nav-link">
+                    Ajouter
+                  </Link>
+                </button>
             </div>
           </div>
         </nav>
 
-        <div className="d-flex flex-column justify-content-start align-items-center" style={{ height: "100vh", backgroundColor: "#b8aaa0" }}>
-          <div class="container">
 
-
-            {/* partie à remplacer par le loop de services */}
-            {loadComponents()}
-
-            <div class="row">
-              <div class="col-lg-8 col-md-10 col-sm-12">
-                <br></br>
-                <div class="card">
-                  <div class="card-body justify-content-center">
-                    <ul></ul>
-                    <h5 class="card-title text-center font-weight-bold">Demander à Milaz le code</h5>
-                    <br></br>
-                    <div class="text-center">
-                      <button 
-                        type="button" 
-                        class="btn btn-primary"
-                        data-id={"btn "}        
-                        onClick={() => console.log("bouton service clic")}>
-                        Choisir
-                      </button>
-                    </div>
-                  </div>
+        {/* Tableau des services */}
+        <div className="container pt-5"> {/* Container for the services */}
+                <div className="row">
+                    {lstTypesOfService && // Check if types of service have been fetched before rendering the services
+                        lstTypesOfService.map((typeOfService) => (
+                            <div className={`col-md-${numCols}`} key={typeOfService.id}> {/* Create a column for each service */}
+                                <Card className="mb-4"> {/* Create a card to display each type of service */}
+                                    <Card.Body>
+                                        <Card.Title style={{ color: "#232627", fontSize: "36px", marginBottom: "22px" }}>{typeOfService.name}</Card.Title> {/* Display the name of the service type */}
+                                        <Card.Subtitle className="mb-2" style={{ color: "#F3B10E", fontSize: "28px" }}>
+                                            À partir de CHF {minPriceForATypeOfService(typeOfService)}.-
+                                        </Card.Subtitle> {/* Display the minimum price for each type of service */}
+                                        <hr />
+                                        <div>
+                                            {lstServices &&
+                                                lstServices
+                                                    .filter((service) => service.type_of_service === typeOfService.id) // Filter services based on their type
+                                                    .sort((a, b) => b.price - a.price) // Sort services by price in descending order
+                                                    .map((service) => (
+                                                        <div
+                                                            className="mb-3 d-flex flex-column"
+                                                            key={service.id}
+                                                            style={{
+                                                                background: "whiteSmoke",
+                                                                borderRadius: "6px",
+                                                                padding: "10px",
+                                                                boxShadow: "0 2px 4px rgba(0,0,0,.2)",
+                                                            }}
+                                                        >
+                                                            <div>
+                                                                <Card.Text className="mb-1">{service.name}</Card.Text> {/* Display the name of the service */}
+                                                                <Card.Text className="mb-2">{formatDuration(service.duration)} minutes, CHF {priceWithoutCent(service.price)}.-</Card.Text> {/* Display the duration and price of the service */}
+                                                            </div>
+                                                            <Button
+                                                                variant="primary"
+                                                                style={{
+                                                                    background: "#232627",
+                                                                    alignSelf: "flex-end",
+                                                                    borderColor: "#232627",
+                                                                    transition: "all 0.2s ease-in-out",
+                                                                }}
+                                                                onClick={() => handleChooseService(service)}
+                                                                onMouseOver={(e) => (e.target.style.background = "#383a3d")}
+                                                                onMouseOut={(e) => (e.target.style.background = "#232627")}
+                                                                disabled={mode}
+                                                            >
+                                                                Choisir
+                                                            </Button> {/* Button to choose a service */}
+                                                        </div>
+                                                    ))}
+                                        </div>
+                                    </Card.Body>
+                                    <Card.Footer className="text-muted" style={{ background: "white", alignSelf: "flex-end", border:"none"}}>
+                                        Étudiant réduc 5.-
+                                    </Card.Footer> {/* Footer displaying a discount for students */}
+                                </Card>
+                            </div>
+                        ))}
                 </div>
-              </div>
             </div>
 
+        {/* Pagination */}
 
-
-
-
-          </div>
-        </div>
-
-        <nav aria-label="Page navigation example">
+        {/* <nav aria-label="Page navigation example">
           <ul class="pagination">
             <li class="page-item"><a class="page-link" href="#">Previous</a></li>
             <li class="page-item"><a class="page-link" href="#">1</a></li>
@@ -134,7 +190,7 @@ export default function Services() {
             <li class="page-item"><a class="page-link" href="#">3</a></li>
             <li class="page-item"><a class="page-link" href="#">Next</a></li>
           </ul>
-        </nav>
+        </nav> */}
 
       </div>
     </>
