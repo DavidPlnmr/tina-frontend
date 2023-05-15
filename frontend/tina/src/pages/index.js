4d5a2b65fe11c2a9208160264f77051b5491221e
Bruno Regadas <bruno.regadas@etu.hesge.ch>
2023-05-15 12:25:58 +0200

    réparation calendrier util

--- frontend/tina/src/pages/index.js
+++ frontend/tina/src/pages/index.js
@@ -1,18 +1,29 @@
-import Head from 'next/head'
-import Header from './components/header'
-import Accueil from './components/accueil'
-
+import Head from "next/head";
+import Header from "./components/header";
+import Accueil from "./components/accueil";
 
 export default function Home() {
   return (
     <>
+      <style type="text/css">
+        {`
+                    .fc-event-main {
+                      background-color: #1AA7EC; /* gris clair */ !important
+                      color: white; 
+                  }
+                  .fc-timegrid-event {
+                      background-color: #1AA7EC; /* gris clair */ !important
+                  }
+                  
+              `}
+      </style>
       <Head>
         <title>Page d'accueil</title>
       </Head>
       <main>
-        <Header/>
-        <Accueil/>
+        <Header />
+        <Accueil />
       </main>
     </>
-  )
+  );
 }
