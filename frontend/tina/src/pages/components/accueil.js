768753b914ec04d30df0e6a16bb149930a61584c
MilazIdrizi <milazim.idrz@gmail.com>
2023-05-15 14:51:17 +0200

    Ajout d'animation lors du scroll + hover effect, mise en place final de la partie 'Qui sommes nous'

--- frontend/tina/src/pages/components/accueil.js
+++ frontend/tina/src/pages/components/accueil.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from "@/styles/Accueil.module.css";
 import Carousel from 'react-bootstrap/Carousel';
 import Footer from "@/pages/components/footer";
+import { Fade } from 'react-awesome-reveal';
 import {Button, Card, Col, Container, Row} from "react-bootstrap";
-import styles from '@/styles/Accueil.module.css'
 
 
 const products = [
@@ -17,142 +18,151 @@ const products = [
 export default function Accueil() {
     return (
         <>
-            <Carousel>
-                <Carousel.Item>
-                    <img src="/images/carousel/carousel_1.jpg" className="d-block w-100 h-50" alt="..." />
-                    <Carousel.Caption>
-                        <h3>Salon de coiffure et barbier</h3>
-                        <p>Profitez d'une expérience exceptionnelle dans notre salon de coiffure et barbier.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img src="/images/carousel/carousel_2.jpg" className="d-block w-100 " height={500} alt="..." />
-                    <Carousel.Caption>
-                        <h3>Des professionnels à votre service</h3>
-                        <p>Découvrez nos services de coiffure et de barbier pour hommes.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img src="/images/carousel/carousel_3.jpg" className="d-block w-100" height={500} alt="..." />
-                    <Carousel.Caption>
-                        <h3>Des produits de qualité</h3>
-                        <p>Utilisation de produits de qualité pour des résultats durables et éclatants.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
+
+
+            <video autoPlay loop muted className={"d-block w-100 position-relative videoFilter"}>
+                <source src="/images/carousel/tina_video.mp4" type="video/mp4" />
+            </video>
+
+
+
 
             {/* Nos services */}
-                <br/>
-            <Container>
-                <h2 className={"text-center text-primary"}>Nos services</h2>
-                <hr className="featurette-divider" />
-                <Row>
-                    <div className="col-md-4">
-                        <div className="card">
-                            <img src="https://i.pinimg.com/736x/ae/67/7a/ae677a18891ad058e1526f2b087028b6.jpg" className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title">Coupe de cheveux personnalisée</h5>
-                                <p>Notre équipe de coiffeurs professionnels est spécialisée dans les coupes de cheveux pour hommes. Que vous souhaitiez un look classique ou un style plus moderne, nous sommes là pour vous aider à trouver la coupe parfaite qui reflète votre personnalité et votre style.</p>
+            <br/>
+            <div className={styles.secondBackgroundColor}>
+                <Container>
+                    <Fade triggerOnce direction="up">
+                        <h2 className={"text-center"}>Nos services</h2>
+                        <hr className="featurette-divider" />
+                        <Row>
+                            <div className="col-md-4">
+
+                                <div className={`card ${styles.servicecard}`}>
+                                    <img src="https://i.pinimg.com/736x/ae/67/7a/ae677a18891ad058e1526f2b087028b6.jpg" className="card-img-top" alt="Coupe de cheveux personnalisée" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Coupe de cheveux personnalisée</h5>
+                                        <p>Notre équipe de coiffeurs professionnels est spécialisée dans les coupes de cheveux pour hommes. Que vous souhaitiez un look classique ou un style plus moderne, nous sommes là pour vous aider à trouver la coupe parfaite qui reflète votre personnalité et votre style.</p>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4">
-                        <div className="card">
-                            <img src="https://img.freepik.com/free-photo/handsome-man-cutting-beard-barber-shop-salon_1303-20931.jpg" className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title">Taille de la barbe et rasage</h5>
-                                <p>Offrez-vous une expérience de rasage et de taille de la barbe de qualité supérieure. Nos barbiers experts sont formés pour sculpter et entretenir votre barbe avec précision, en utilisant des outils et des techniques traditionnelles pour un résultat impeccable.</p>
+                            <div className="col-md-4">
+                                <div className={`card ${styles.servicecard}`}>
+                                    <img src="https://img.freepik.com/free-photo/handsome-man-cutting-beard-barber-shop-salon_1303-20931.jpg" className="card-img-top" alt="Taille de la barbe et rasage" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Taille de la barbe et rasage</h5>
+                                        <p>Offrez-vous une expérience de rasage et de taille de la barbe de qualité supérieure. Nos barbiers experts sont formés pour sculpter et entretenir votre barbe avec précision, en utilisant des outils et des techniques traditionnelles pour un résultat impeccable.</p>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4">
-                        <div className="card">
-                            <img src="https://img.freepik.com/free-photo/crop-hairdresser-washing-hair-client_23-2147778780.jpg?w=1060&t=st=1683727994~exp=1683728594~hmac=3374aec0a5e05c5d2ecdc7fe13a9f1f3a1460f58533de981c23e6ad5442d5e56" className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title">Coloration</h5>
-                                <p>Notre barber shop propose des services de coloration personnalisés pour les hommes, allant de la couverture des cheveux gris à des transformations audacieuses. Nous utilisons des produits de haute qualité et respectueux du cuir chevelu pour garantir des résultats durables et éclatants.</p>
+                            <div className="col-md-4">
+                                <div className={`card ${styles.servicecard}`}>
+                                    <img src="https://img.freepik.com/free-photo/crop-hairdresser-washing-hair-client_23-2147778780.jpg?w=1060&t=st=1683727994~exp=1683728594~hmac=3374aec0a5e05c5d2ecdc7fe13a9f1f3a1460f58533de981c23e6ad5442d5e56" className="card-img-top" alt="Coloration" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Coloration</h5>
+                                        <p>Notre barber shop propose des services de coloration personnalisés pour les hommes, allant de la couverture des cheveux gris à des transformations audacieuses. Nous utilisons des produits de haute qualité et respectueux du cuir chevelu pour garantir des résultats durables et éclatants.</p>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                </Row>
-            </Container>
+                        </Row>
+                    </Fade>
+                </Container>
+            </div>
+
             <br/><br/>
 
             {/* Qui sommes-nous ?*/}
-            <Container>
-                <br/>
-                <Row>
-                    <div className="col-md-6">
-                        <img className="img-fluid" src="/images/carousel/carousel_1.jpg" alt="..." />
-                        {/*button on image*/}
-                        <div className="centered">
-                            <Button variant="outline-light">Qui sommes-nous ?</Button>
-                        </div>
-                    </div>
-                    <div className="col-md-6">
-                        <h2>Qui sommes-nous ?</h2>
-                        <p>Nous sommes une équipe de barbiers et de coiffeurs passionnés, engagés à offrir les meilleurs services pour les hommes dans une atmosphère détendue et accueillante. Nous comprenons l'importance d'une apparence soignée et d'un style qui reflète la personnalité de chacun. C'est pourquoi nous mettons tout en œuvre pour comprendre les besoins de nos clients et leur offrir des résultats impeccables.</p>
-
-                        <p>Notre gamme de services inclut des coupes de cheveux sur mesure, la taille et le rasage de la barbe, la coloration, les soins du visage pour hommes et bien d'autres prestations, conçues spécifiquement pour répondre aux besoins de notre clientèle masculine. Nous utilisons des produits de qualité supérieure et des techniques à la pointe de la mode pour garantir que vous repartiez de notre salon avec un look qui vous met en valeur et vous donne confiance en vous.</p>
-
-                        <p>Chez Tina Coiffure, nous croyons que chaque homme mérite une expérience de salon digne de ce nom. Venez nous rendre visite et découvrez pourquoi notre réputation d'excellence et de service client irréprochable fait de nous le barber shop de choix pour les hommes soucieux de leur apparence et de leur bien-être. Nous vous attendons avec impatience pour vous offrir l'expérience ultime en matière de coiffure et de soins pour hommes.</p>
-                    </div>
-                </Row>
-            </Container>
-
-            {/* Nos produits*/}
-            <br/><br/>
-            <Container>
-                <h2 className="text-center">Nos produits</h2>
-                <hr className="featurette-divider" />
-                <br />
-
-                <Row className="row-cols-1 row-cols-md-3 g-4">
-                    {products.map((product, index) => (
-                        <div className="col" key={index}>
-                            <div className="card-container">
-                                <Card className="custom-card h-100" style={{ backgroundColor: product.backgroundColor }}>
-                                    <Card.Body className="d-flex align-items-center justify-content-center">
-                                        <Card.Title className="custom-card-title" style={{ color: product.textColor }}>
-                                            {product.title}
-                                        </Card.Title>
-                                    </Card.Body>
-                                </Card>
+            <div class="bg-white py-5">
+                <Fade triggerOnce direction="up">
+                    <div class="container py-5">
+                        <div class="row align-items-center mb-5">
+                            <div class="col-lg-6 order-2 order-lg-1"><i class="fa fa-bar-chart fa-2x mb-3 text-primary"></i>
+                                <h2 class="font-weight-light">Notre philosophie</h2>
+                                <p class="font-italic text-muted mb-4">Chez Tina Coiffure, nous croyons que chaque coupe de cheveux est une forme d'expression. Notre mission est d'aider chaque client à trouver son style unique, à travers des services personnalisés et une attention particulière aux détails. Nous sommes passionnés par notre métier et nous nous efforçons de créer une expérience agréable et mémorable à chaque visite.</p>
+                                <a href="https://goo.gl/maps/jHu3JBzorevPJ9qf6" target="_blank" className="btn btn-light px-5 rounded-pill shadow-sm">Où nous trouver ?</a>
+                            </div>
+                            <div class="col-lg-5 px-5 mx-auto order-1 order-lg-2">
+                                <img
+                                src="https://i.pinimg.com/736x/ae/67/7a/ae677a18891ad058e1526f2b087028b6.jpg" alt=""
+                                className="img-fluid mb-4 mb-lg-0 rounded-pill"></img>
                             </div>
                         </div>
-                    ))}
-                </Row>
-            </Container>
+                        <div class="row align-items-center">
+                            <div class="col-lg-5 px-5 mx-auto">
+                                <img
+                                src="https://img.freepik.com/free-photo/handsome-man-cutting-beard-barber-shop-salon_1303-20931.jpg" alt=""
+                                className="img-fluid mb-4 mb-lg-0 rounded-pill"></img>
+                            </div>
+                            <div class="col-lg-6"><i class="fa fa-leaf fa-2x mb-3 text-primary"></i>
+                                <h2 class="font-weight-light">Une tradition revisitée</h2>
+                                <p class="font-italic text-muted mb-4">Chez Tina Coiffure, nous honorons la tradition du barber shop tout en l'adaptant aux exigences modernes. Nous combinons l'artisanat classique de la coiffure masculine avec les dernières tendances et techniques. Que vous recherchiez une coupe de cheveux classique, une taille de barbe précise ou un style plus audacieux, nous avons les compétences et l'expérience nécessaires pour répondre à vos besoins.</p>
+                                <a href="https://goo.gl/maps/jHu3JBzorevPJ9qf6" target="_blank" className="btn btn-light px-5 rounded-pill shadow-sm">Contactez-nous</a>
+                            </div>
+                        </div>
+                    </div>
+                </Fade>
+            </div>
+
+
+            {/* Nos produits*/}
+            <div className={styles.fourthBackgroundColorGradient}>
+                <br/>
+                <Container>
+                    <Fade triggerOnce direction="up">
+                        <h2 className="text-center">Nos produits</h2>
+                        <hr className="featurette-divider" />
+                        <br />
+
+                        <Row className="row-cols-1 row-cols-md-3 g-4">
+                            {products.map((product, index) => (
+                                <div className="col" key={index}>
+                                    <div className="card-container">
+                                        <Card className="custom-card h-100" style={{ backgroundColor: product.backgroundColor }}>
+                                            <Card.Body className="d-flex align-items-center justify-content-center">
+                                                <Card.Title className="custom-card-title" style={{ color: product.textColor }}>
+                                                    {product.title}
+                                                </Card.Title>
+                                            </Card.Body>
+                                        </Card>
+                                    </div>
+                                </div>
+                            ))}
+                        </Row>
+                    </Fade>
+                </Container>
+                <br/>
+            </div>
 
             {/*adresse - email - phone*/}
-            <br/><br/>
-            <Container>
-                <br />
-                <Row>
-                    <div className="col-md-6">
-                        <Row>
-                            <Col xs={4} md={2}><p><strong>ADRESSE</strong></p></Col>
-                            <Col xs={1} md={1}><p>:</p></Col>
-                            <Col xs={7} md={9}><a className={"text-dark text-decoration-none text-decoration-underline"} target="_blank" href="https://goo.gl/maps/jHu3JBzorevPJ9qf6">Bd Carl-Vogt 47, 1205 Genève</a></Col>
-                        </Row>
-                    </div>
-                </Row>
-                <Row>
-                    <div className="col-md-6">
-                        <Row>
-                            <Col xs={4} md={2}><p><strong>EMAIL</strong></p></Col>
-                            <Col xs={1} md={1}><p>:</p></Col>
-                            <Col xs={7} md={9}><a className={"text-dark text-decoration-none"} href="mailto:tina.coiffure@hotmail.com">tina.coiffure@hotmail.com</a></Col>
-                        </Row>
-                        <Row>
-                            <Col xs={4} md={2}><p><strong>PHONE</strong></p></Col>
-                            <Col xs={1} md={1}><p>:</p></Col>
-                            <Col xs={7} md={9}><a className={"text-dark text-decoration-none"} href="tel:+41223202222">+41 22 320 22 22</a></Col>
-                        </Row>
-                    </div>
-                </Row>
-                <br/><br/>
-            </Container>
+            <div className="bg-dark text-light py-3">
+                <br/>
+                <Container className="text-muted">
+                    <br />
+                    <Row>
+                        <div className="col-md-6">
+                            <Row>
+                                <Col xs={4} md={2}><p><strong>ADRESSE</strong></p></Col>
+                                <Col xs={1} md={1}><p>:</p></Col>
+                                <Col xs={7} md={9}><a className={"text-muted text-decoration-none text-decoration-underline"} target="_blank" href="https://goo.gl/maps/jHu3JBzorevPJ9qf6">Bd Carl-Vogt 47, 1205 Genève</a></Col>
+                            </Row>
+                        </div>
+                    </Row>
+                    <Row>
+                        <div className="col-md-6">
+                            <Row>
+                                <Col xs={4} md={2}><p><strong>EMAIL</strong></p></Col>
+                                <Col xs={1} md={1}><p>:</p></Col>
+                                <Col xs={7} md={9}><a className={"text-muted text-decoration-none"} href="mailto:tina.coiffure@hotmail.com">tina.coiffure@hotmail.com</a></Col>
+                            </Row>
+                            <Row>
+                                <Col xs={4} md={2}><p><strong>PHONE</strong></p></Col>
+                                <Col xs={1} md={1}><p>:</p></Col>
+                                <Col xs={7} md={9}><a className={"text-muted text-decoration-none"} href="tel:+41223202222">+41 22 320 22 22</a></Col>
+                            </Row>
+                        </div>
+                    </Row>
+                    <hr className="featurette-divider" />
+                </Container>
+            </div>
             {/*Footer*/}
             <Footer />
         </>
