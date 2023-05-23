02795c17ded3adebf93cf9942be1cde77cb7c139
mila_ <milazim.idrz@gmail.com>
2023-05-23 20:24:19 +0200

    fix langue par defaut

--- frontend/tina/src/pages/components/identification/connexion.js
+++ frontend/tina/src/pages/components/identification/connexion.js
@@ -76,71 +76,74 @@ export default function Connexion() {
   };
 
   return (
-    <>
-      <div
-        className="d-flex justify-content-center align-items-center"
-        style={{ height: "100vh", backgroundColor: "#b8aaa0" }}
-      >
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-sm-10 col-md-8 col-lg-6">
-              <Card
-                className="border-0"
-                style={{ backgroundColor: "#b8aaa0", marginTop: "-150px" }}
-              >
-                <Card.Body>
-                  <h2 className="text-center mb-4">Tina Coiffure</h2>
-                  <Card.Title className="text-center mb-4">
-                    Connexion
-                  </Card.Title>
-                  <Form onSubmit={handleSubmit}>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                      <Form.Control
-                        data-id="username"
-                        value={user.username}
-                        type="text"
-                        placeholder="Nom d'utilisateur"
-                        onChange={handleChange}
-                      />
-                    </Form.Group>
+      <>
+        <div
+            className="d-flex justify-content-center align-items-center"
+            style={{ height: "100vh", backgroundColor: "#b8aaa0" }}
+        >
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-sm-10 col-md-8 col-lg-6">
+                <Card
+                    className="border-0"
+                    style={{ backgroundColor: "#b8aaa0", marginTop: "-150px" }}
+                >
+                  <Card.Body>
+                    <Card.Title className="text-center mb-4">
+                      <div className="d-flex justify-content-center align-items-center" style={{gap: "10px", fontFamily: "Abhaya Libre", fontSize: "38px", fontWeight: "bold"}}>
+                        <img src="/images/tina_logo.png" className="shadow" width="50" alt="logo" />
+                        <h2>Tina Coiffure</h2>
+                      </div>
+                    </Card.Title>
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                      <Form.Control
-                        data-id="password"
-                        value={user.password}
-                        type="password"
-                        placeholder="Mot de passe"
-                        onChange={handleChange}
-                      />
+                    <Form onSubmit={handleSubmit}>
+                      <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Control
+                            data-id="username"
+                            value={user.username}
+                            type="text"
+                            placeholder="Nom d'utilisateur"
+                            onChange={handleChange}
+                        />
+                      </Form.Group>
+
+                      <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Control
+                            data-id="password"
+                            value={user.password}
+                            type="password"
+                            placeholder="Mot de passe"
+                            onChange={handleChange}
+                        />
+                        <Form.Text className="text-muted">
+                          <Link class="nav-link" href="/">
+                            Problème de connexion ?
+                          </Link>
+                        </Form.Text>
+                      </Form.Group>
+                      <Button
+                          variant="primary"
+                          type="submit"
+                          className='w-100 border-0"'
+                          style={{ backgroundColor: "#232627", border: 0 }}
+                      >
+                        Se connecter
+                      </Button>
                       <Form.Text className="text-muted">
-                        <Link class="nav-link" href="/">
-                          Problème de connexion ?
+                        <Link
+                            class="nav-link"
+                            href="/components/identification/inscription"
+                        >
+                          Vous n'avez pas de compte ?
                         </Link>
                       </Form.Text>
-                    </Form.Group>
-                    <Button
-                      variant="primary"
-                      type="submit"
-                      className='w-100 border-0"'
-                      style={{ backgroundColor: "#232627", border: 0 }}
-                    >
-                      Se connecter
-                    </Button>
-                    <Form.Text className="text-muted">
-                      <Link
-                        class="nav-link"
-                        href="/components/identification/inscription"
-                      >
-                        Vous n'avez pas de compte ?
-                      </Link>
-                    </Form.Text>
-                  </Form>
-                </Card.Body>
-              </Card>
+                    </Form>
+                  </Card.Body>
+                </Card>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
+      </>
   );
 }
