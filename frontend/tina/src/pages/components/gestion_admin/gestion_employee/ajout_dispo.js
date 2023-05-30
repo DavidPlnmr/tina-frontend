0fa64839f23fdf10670a47f5dd2bc70368d856e2
mila_ <milazim.idrz@gmail.com>
2023-05-31 00:03:10 +0200

    fin rdv_employee.js

--- frontend/tina/src/pages/components/gestion_admin/gestion_employee/ajout_dispo.js
+++ frontend/tina/src/pages/components/gestion_admin/gestion_employee/ajout_dispo.js
@@ -12,138 +12,138 @@ import Footer from "@/pages/components/footer";
 
 export default function AjoutDispo() {
 
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
-  const [dispo, setDispo] = useState([
-    {
-      date: "",
-      start_hour: "",
-      end_hour: "",
-      employee: "",
-      vacation: ""
-    }
-  ]);
+    const [dispo, setDispo] = useState([
+        {
+            date: "",
+            start_hour: "",
+            end_hour: "",
+            employee: "",
+            vacation: ""
+        }
+    ]);
 
-  const  [employees, setEmployees] = useState([]);
-  const router = useRouter();
+    const  [employees, setEmployees] = useState([]);
+    const router = useRouter();
 
-  const fetchEmployees = () => {
-    const cookies = parseCookies();
-    axios.get(baseUrl + 'employees/', {
-      headers: {
-        Authorization: `Token ` + cookies.csrftoken,
-      },
-    })
-        .then((response) => {
-              setEmployees(response.data);
-              if (dispo.employee === undefined){
-                setDispo({ ...dispo, employee: response.data[0].id});
-              }
-            }
-        )
-        .catch((error) => {
-              console.log(error);
-            }
-        );
-  }
-
-
-  useEffect(() => {
-    fetchEmployees();
-  } , []);
-
-  const handleSubmit =  (evt) => {
-    const cookies = parseCookies();
-
-    evt.preventDefault();
-
-    console.log(dispo.employee);
-
-    axios.post(baseUrl + 'employees/' + dispo.employee +'/disponibilities/', dispo, {
-      headers: {
-        Authorization: `Token ` + cookies.csrftoken,
-      },
-    })
-        .then((response) => {
-          console.log(response);
-          console.log(response.data);
-          if (response.data.vacation === false){
-            alert("Disponibilité ajoutée");
-          } else {
-            alert("Vacances ajoutées");
-          }
-          router.push("/components/gestion_admin/dash_admin");
+    const fetchEmployees = () => {
+        const cookies = parseCookies();
+        axios.get(baseUrl + 'employees/', {
+            headers: {
+                Authorization: `Token ` + cookies.csrftoken,
+            },
         })
-        .catch((error) => {
-          console.log(error);
-        });
+            .then((response) => {
+                    setEmployees(response.data);
+                    if (dispo.employee === undefined){
+                        setDispo({ ...dispo, employee: response.data[0].id});
+                    }
+                }
+            )
+            .catch((error) => {
+                    console.log(error);
+                }
+            );
+    }
 
-  }
 
-  const handleChange = (evt) => {
-    setDispo({ ...dispo, [evt.target.dataset.id]: evt.target.value});
-  };
+    useEffect(() => {
+        fetchEmployees();
+    } , []);
 
-  return (
-      <>
-        <Head>
-          <meta charSet="utf-8"/>
-          <title>Tina - Ajout d'une disponibilité ou de vacances pour un employé</title>
-        </Head>
-        <Header/>
-        <main>
-          <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
-            <div className="container">
-              <div className="row justify-content-center">
-                <div className="col-lg-6">
-                  <Card className="shadow-lg p-3 bg-body rounded mt-n5">
-                    <Card.Body>
-                      <h2 className="text-center mb-4">Tina Coiffure</h2>
-                      <Card.Title className="text-center mb-4">Ajout d'une disponibilité ou de vacances pour un employé</Card.Title>
-                      <Form onSubmit={handleSubmit}>
-                        <Form.Group className="mb-3">
-                          <Form.Label>Date</Form.Label>
-                          <Form.Control data-id="date" type="date" value={dispo.date} onChange={handleChange} required/>
-                        </Form.Group>
+    const handleSubmit =  (evt) => {
+        const cookies = parseCookies();
 
-                        <Form.Group className="mb-3">
-                          <Form.Label>Heure de début</Form.Label>
-                          <Form.Control data-id="start_hour" type="time" value={dispo.start_hour} onChange={handleChange} required/>
-                        </Form.Group>
+        evt.preventDefault();
 
-                        <Form.Group className="mb-3">
-                          <Form.Label>Heure de fin</Form.Label>
-                          <Form.Control data-id="end_hour" type="time" value={dispo.end_hour} onChange={handleChange} required/>
-                        </Form.Group>
+        console.log(dispo.employee);
 
-                        <Form.Group className="mb-3">
-                          <Form.Label>Employé</Form.Label>
-                          <Form.Select data-id="employee" onChange={handleChange} required>
-                            {employees.map((employee) => (
-                                <option value={employee.id}>{employee.first_name} {employee.last_name} {"(" + employee.username + ")"}</option>
-                            ))}
-                          </Form.Select>
-                        </Form.Group>
+        axios.post(baseUrl + 'employees/' + dispo.employee +'/disponibilities/', dispo, {
+            headers: {
+                Authorization: `Token ` + cookies.csrftoken,
+            },
+        })
+            .then((response) => {
+                console.log(response);
+                console.log(response.data);
+                if (response.data.vacation === false){
+                    alert("Disponibilité ajoutée");
+                } else {
+                    alert("Vacances ajoutées");
+                }
+                router.push("/components/gestion_admin/dash_admin");
+            })
+            .catch((error) => {
+                console.log(error);
+            });
 
-                        <Form.Group className="mb-3">
-                          <Form.Label>Qu'êtes vous en train d'ajouter ?</Form.Label>
-                          <Form.Check type='radio' data-id="vacation" name="vacation" label="Vacances" value={1} onChange={handleChange} />
-                          <Form.Check type='radio' data-id="vacation" name="vacation" label="Disponibilités" value={0} onChange={handleChange} required/>
-                        </Form.Group>
+    }
 
-                        <Button variant="primary" type="submit" className='w-100'>
-                          Ajouter
-                        </Button>
-                        <Link href='./ajout_multiple_dispo' className="nav-link text-center mt-3">Vous voulez en ajouter plusieurs ? cliquez ici !</Link>
-                      </Form>
-                    </Card.Body>
-                  </Card>
+    const handleChange = (evt) => {
+        setDispo({ ...dispo, [evt.target.dataset.id]: evt.target.value});
+    };
+
+    return (
+        <>
+            <Head>
+                <meta charSet="utf-8"/>
+                <title>Tina - Ajout d'une disponibilité ou de vacances pour un employé</title>
+            </Head>
+            <Header/>
+            <main>
+                <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            <div className="col-lg-6">
+                                <Card className="shadow-lg p-3 bg-body rounded mt-n5">
+                                    <Card.Body>
+                                        <h2 className="text-center mb-4">Tina Coiffure</h2>
+                                        <Card.Title className="text-center mb-4">Ajout d'une disponibilité ou de vacances pour un employé</Card.Title>
+                                        <Form onSubmit={handleSubmit}>
+                                            <Form.Group className="mb-3">
+                                                <Form.Label>Date</Form.Label>
+                                                <Form.Control data-id="date" type="date" value={dispo.date} onChange={handleChange} required/>
+                                            </Form.Group>
+
+                                            <Form.Group className="mb-3">
+                                                <Form.Label>Heure de début</Form.Label>
+                                                <Form.Control data-id="start_hour" type="time" value={dispo.start_hour} onChange={handleChange} required/>
+                                            </Form.Group>
+
+                                            <Form.Group className="mb-3">
+                                                <Form.Label>Heure de fin</Form.Label>
+                                                <Form.Control data-id="end_hour" type="time" value={dispo.end_hour} onChange={handleChange} required/>
+                                            </Form.Group>
+
+                                            <Form.Group className="mb-3">
+                                                <Form.Label>Employé</Form.Label>
+                                                <Form.Select data-id="employee" onChange={handleChange} required>
+                                                    {employees.map((employee) => (
+                                                        <option value={employee.id}>{employee.first_name} {employee.last_name} {"(" + employee.username + ")"}</option>
+                                                    ))}
+                                                </Form.Select>
+                                            </Form.Group>
+
+                                            <Form.Group className="mb-3">
+                                                <Form.Label>Qu'êtes vous en train d'ajouter ?</Form.Label>
+                                                <Form.Check type='radio' data-id="vacation" name="vacation" label="Vacances" value={1} onChange={handleChange} />
+                                                <Form.Check type='radio' data-id="vacation" name="vacation" label="Disponibilités" value={0} onChange={handleChange} required/>
+                                            </Form.Group>
+
+                                            <Button variant="primary" type="submit" className='w-100'>
+                                                Ajouter
+                                            </Button>
+                                            <Link href='./ajout_multiple_dispo' className="nav-link text-center mt-3">Vous voulez en ajouter plusieurs ? cliquez ici !</Link>
+                                        </Form>
+                                    </Card.Body>
+                                </Card>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </main>
-        <Footer/>
-      </>
-  );
+            </main>
+            <Footer/>
+        </>
+    );
 }
--- frontend/tina/src/pages/components/prise_rendez_vous/rdv_employee.js
+++ frontend/tina/src/pages/components/prise_rendez_vous/rdv_employee.js
@@ -1,11 +1,12 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import Header from "../header";
 import { parseCookies } from "nookies";
 import axios from "axios";
-import Router from "next/router";
 import { useRouter } from "next/router";
 import Footer from "../footer";
 import Head from "next/head";
+import {Button, Card, Container, Row} from "react-bootstrap";
+import { FaUserAlt } from 'react-icons/fa';
 
 /**
  * @namespace 'Rdv_employee.js'
@@ -14,142 +15,127 @@ import Head from "next/head";
  */
 export default function Rdv_employee() {
 
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
-  /**
-   * @memberof 'Rdv_employee.js'
-   * @constant urlEmployees
-   * @description URL of the API to fetch the list of employees
-   */
-  const urlEmployees = baseUrl + "employees/"
+    /**
+     * @memberof 'Rdv_employee.js'
+     * @constant urlEmployees
+     * @description URL of the API to fetch the list of employees
+     */
+    const urlEmployees = baseUrl + "employees/"
 
-  /**
-   * @memberof 'Rdv_employee.js'
-   * @constant urlNextPage
-   * @description URL of the next page to redirect to
-   */
-  const urlNextPage = "/components/prise_rendez_vous/calendrier"
+    /**
+     * @memberof 'Rdv_employee.js'
+     * @constant urlNextPage
+     * @description URL of the next page to redirect to
+     */
+    const urlNextPage = "/components/prise_rendez_vous/calendrier"
 
-  /**
-   * @memberof 'Rdv_employee.js'
-   * @constant router
-   * @see {@link 'header.js'.router}
-   */
-  const router = useRouter();
-  /**
-   * @memberof 'Rdv_employee.js'
-   * @constant service_json
-   * @description Service selected by the user, passed as a query parameter
-   */
-  const service_json = router.query;
-  /**
-   * @memberof 'Rdv_employee.js'
-   * @constant lstEmployee
-   * @description List of employees fetched from the API
-   * @see {@link 'Rdv_employee.js'.fetchEmployee}
-   * @default []
-   */
-  const [lstEmployee, setLstEmployee] = useState([]);
-  /**
-   * @memberof 'Rdv_employee.js'
-   * @constant lstCompEmployee
-   * @description List of employees to be displayed
-   * @see {@link 'Rdv_employee.js'.loadEmployee}
-   * @default []
-   */
-  let lstCompEmployee = [];
- 
+    /**
+     * @memberof 'Rdv_employee.js'
+     * @constant router
+     * @see {@link 'header.js'.router}
+     */
+    const router = useRouter();
+    /**
+     * @memberof 'Rdv_employee.js'
+     * @constant service_json
+     * @description Service selected by the user, passed as a query parameter
+     */
+    const service_json = router.query;
+    /**
+     * @memberof 'Rdv_employee.js'
+     * @constant lstEmployee
+     * @description List of employees fetched from the API
+     * @see {@link 'Rdv_employee.js'.fetchEmployee}
+     * @default []
+     */
+    const [lstEmployee, setLstEmployee] = useState([]);
+    /**
+     * @memberof 'Rdv_employee.js'
+     * @function fetchEmployee
+     * @description Fetch the list of employees from the API
+     */
+    const fetchEmployee = () => {
+        const cookies = parseCookies();
+        axios
+            .get(urlEmployees, {
+                headers: {
+                    Authorization: "Token " + cookies.csrftoken,
+                },
+            })
+            .then((response) => {
+                setLstEmployee(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
 
-  /**
-   * @memberof 'Rdv_employee.js'
-   * @function fetchEmployee
-   * @description Fetch the list of employees from the API
-   */
-  const fetchEmployee = () => {
-    const cookies = parseCookies();
-    axios
-      .get(urlEmployees, {
-        headers: {
-          Authorization: "Token " + cookies.csrftoken,
-        },
-      })
-      .then((response) => {
-        setLstEmployee(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+    const handleOnClick = (e) => {
+        router.push({
+            pathname: urlNextPage,
+            query: { employee: JSON.stringify(e), service: service_json.service },
+        });
+    };
 
-  const handleOnClick = (e) => {
-    router.push({
-      pathname: urlNextPage,
-      query: { employee: JSON.stringify(e), service: service_json.service },
-    });
-  };
 
-  const loadEmployee = () => {
-    lstEmployee.map((e) => {
-      lstCompEmployee.push(
-        <div key={e.id} className="col-lg-3 col-md-6 col-sm-12">
-          <br></br>
-          <div className="card">
-            <div className="card-body justify-content-center">
-              <ul></ul>
-              <h5 className="card-title text-center font-weight-bold">
-                {e.first_name} {e.last_name}
-              </h5>
-              <br></br>
-              <div className="text-center">
-                <button
-                  type="button"
-                  className="btn btn-primary"
-                  data-id={"btn " + e.id}
-                  style={{ backgroundColor: "#232627", border: "none" }}
-                  onClick={() => handleOnClick(e)}
-                >
-                  Choisir
-                </button>
-              </div>
+    /**
+     * @memberof 'Rdv_employee.js'
+     * @function useEffect
+     * @description on page load, fetch the list of employees, if the router is ready
+     */
+    useEffect(() => {
+        if (!router.isReady) { return; }
+        fetchEmployee();
+
+    }, [router.isReady]);
+
+    /**
+     * @memberof 'Rdv_employee.js'
+     * @function loadEmployee
+     * @description Load the list of employees to be displayed
+     * @see {@link 'Rdv_employee.js'.lstEmployee}
+     * @see {@link 'Rdv_employee.js'.handleOnClick}
+     * @return {JSX.Element}
+     */
+    const loadEmployee = () => {
+        return lstEmployee.map((e) => (
+            <div key={e.id} className="col-lg-3 col-md-6 col-sm-12 mb-4">
+                <Card className="h-100 shadow bg-white rounded">
+                    <Card.Body className="d-flex flex-column align-items-center text-center">
+                        <div className="d-flex mb-2 justify-content-center">
+                            <FaUserAlt size={70} className="align-self-center text-primary mt-3"/>
+                        </div>
+                        <Card.Title className="mb-2 text-dark">
+                            {e.first_name} {e.last_name}
+                        </Card.Title>
+                        <Button variant="dark" className="mt-auto font-weight-bold w-75 align-self-center" data-id={"btn " + e.id} onClick={() => handleOnClick(e)}>
+                            Choisir
+                        </Button>
+                    </Card.Body>
+                </Card>
             </div>
-          </div>
-        </div>
-      );
-    });
-    return lstCompEmployee;
-  };
+        ));
+    };
 
-  /**
-   * @memberof 'Rdv_employee.js'
-   * @function useEffect
-   * @description on page load, fetch the list of employees, if the router is ready
-   */
-  useEffect(() => {
-    if (!router.isReady) { return; }
-    fetchEmployee();
-
-  }, [router.isReady]);
-
-  return (
-    <>
-      <Head>
-        <title>Tina - Prise de rendez-vous</title>
-        <meta name="description" content="Page de prise de rendez-vous de l'application Tina" />
-      </Head>
-      <Header />
-      <br></br>
-
-      <div>
-        <main>
-          <div className="container">
-            <h2>Sélectionnez un coiffeur :</h2>
-            <div className="row">{loadEmployee()}</div>
-          </div>
-        </main>
-      </div>
-
-      <Footer />
-
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>Tina - Prise de rendez-vous</title>
+                <meta name="description" content="Page de prise de rendez-vous de l'application Tina" />
+            </Head>
+            <Header />
+            <main className="my-5">
+                <Container>
+                    <div className="text-center mb-5">
+                        <h4 className="display-4 fw-bold">Sélectionnez un coiffeur</h4>
+                        <hr className="border-top border-dark w-25 mx-auto my-4"/>
+                    </div>
+                    <Row>{loadEmployee()}</Row>
+                </Container>
+            </main>
+            <Footer />
+        </>
+    );
 }
