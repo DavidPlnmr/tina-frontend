59f6deee7086acb5b4ed08a5a730a16c95ec96f3
mila_ <milazim.idrz@gmail.com>
2023-05-31 22:27:47 +0200

    pas de rdv visuelle

--- frontend/tina/src/pages/components/prise_rendez_vous/pas_rdv.js
+++ frontend/tina/src/pages/components/prise_rendez_vous/pas_rdv.js
@@ -10,45 +10,49 @@ import Footer from "../footer";
  */
 export default function ConfirmationRdv() {
 
-  /**
-   * @constant router
-   * @memberof 'confirmation_rdv.js'
-   * @see {@link 'header.js'.router}
-   */
-  const router = useRouter();
+    /**
+     * @constant router
+     * @memberof 'confirmation_rdv.js'
+     * @see {@link 'header.js'.router}
+     */
+    const router = useRouter();
 
-  /**
-   * @function handleClick
-   * @memberof 'confirmation_rdv.js'
-   * @description A function that redirects to the home page.
-   * @returns {void}
-   */
-  const handleClick = () => {
-    router.push("./service_rdv");
-  };
-  return (
-    <>
-      <Head>
-        <title>Tina - Confirmation de rendez-vous</title>
-        <meta name="description" content="Confirmation de rendez-vous" />
-      </Head>
-      <Header />
-      <main>
-      <div className="container py-5 jumbotron" style={{ marginTop: "200px" }}>
-        <h1 className="text-center mb-5">Cet employé n'a pas de disponibilités !</h1>
-        <div className="d-grid gap-3 col-6 mx-auto">
-          <button
-            className="btn btn-primary"
-            type="button"
-            onClick={handleClick}
-            style={{ backgroundColor: "#232627" }}
-          >
-            Retour en arrière
-          </button>
-        </div>
-      </div>
-      </main>
-      <Footer />
-    </>
-  );
+    /**
+     * @function handleClick
+     * @memberof 'confirmation_rdv.js'
+     * @description A function that redirects to the home page.
+     * @returns {void}
+     */
+    const handleClick = () => {
+        router.push("./service_rdv");
+    };
+    return (
+        <>
+            <Head>
+                <title>Tina - Confirmation de rendez-vous</title>
+                <meta name="description" content="Confirmation de rendez-vous" />
+            </Head>
+            <Header />
+            <main>
+                <div
+                    className="container py-5 jumbotron rounded shadow-lg text-center"
+                    style={{ marginTop: "200px", backgroundColor: "#f8f9fa" }}  // light grey background
+                >
+                    <h1 className="mb-5">Cet employé n'a pas de disponibilités !</h1>
+                    <div className="d-grid gap-3 col-6 mx-auto">
+                        <button
+                            className="btn btn-primary btn-lg"  // larger button
+                            type="button"
+                            onClick={handleClick}
+                            style={{ backgroundColor: "#232627" }}
+                        >
+                            Retour en arrière
+                        </button>
+                    </div>
+                </div>
+            </main>
+            <Footer />
+        </>
+    );
+
 }
